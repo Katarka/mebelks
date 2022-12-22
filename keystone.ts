fb70c1ca3512cb6a05d0302eeb7bb9c24c4c3c11
gmx <www.runit@gmail.com>
2022-12-23 00:33:41 +0300

    Initial commit

---
+++ keystone.ts
@@ -0,0 +1,22 @@
+import {config} from '@keystone-6/core';
+
+import {lists} from './schema';
+
+import {withAuth, session} from './auth';
+
+export default withAuth(
+    config({
+        server: {
+            cors: {origin: ['http://127.0.0.1:5173'], credentials: true},
+            port: 4000,
+        },
+        db: {
+            provider: 'postgresql',
+            url: 'postgres://j69129535_mebel:lollipop55O!@postgresql.j69129535.myjino.ru/j69129535_mebel',
+            onConnect: async context => {
+            }
+        },
+        lists,
+        session,
+    })
+);
