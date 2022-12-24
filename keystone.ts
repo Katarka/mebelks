e46f8f58dace9af35a6a51e96cfe628a5cfe4e5d
gmx <www.runit@gmail.com>
2022-12-24 23:41:53 +0300

    fix cors

--- keystone.ts
+++ keystone.ts
@@ -6,10 +6,6 @@ import {withAuth, session} from './auth';
 
 export default withAuth(
     config({
-        server: {
-            cors: {origin: ['*'], credentials: true},
-            port: 4000,
-        },
         db: {
             provider: 'postgresql',
             url: 'postgres://j69129535_mebel:lollipop55O!@postgresql.j69129535.myjino.ru/j69129535_mebel',
